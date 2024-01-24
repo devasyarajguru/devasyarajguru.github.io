c08cea77968925f2c6d036c082f6b9a91c8d5aa2
Devasya Rajguru <84851506+devasyarajguru@users.noreply.github.com>
2024-01-24 15:52:42 +0000

    set Homepage for my portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Devasya Rajguru",
+  title: "Frontend Developer",
+  email: "devasyarajguru@gmail.com",
+  gitHub: "devasyarajguru",
+  instagram: "",
+  linkedIn: "devasya-rajguru-1a38b0211",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "@RajguruDevasya",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
